c0f8fe050efbc5c41e47bab2f395f2d24bb9c249
sadityaprdxn <adityas@prdxn.com>
2020-07-15 16:59:22 +0530

    validate user

--- src/components/Loginpage.js
+++ src/components/Loginpage.js
@@ -1,25 +1,81 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
     BrowserRouter as Router,
     Route,
     Switch,
     useParams,
     useHistory
-} from 'react-router-dom'
+} from 'react-router-dom';
+import Data from '../data/userdata.json';
 
 const Loginpage = ({login}) => {
     
     const history = useHistory();
+    const [allFieldsRight , setField] = useState({usernameField: null , passwordField: null});
+    const [values , setValues] = useState({username : "", password: ""})
 
-    const userLogin = () => {
-        login();
-        history.push('/landingpage');
+    const userLogin = (e) => {
+        e.preventDefault();
+
+        for(let i = 0; i < Data.length; i++){
+
+            const {username, password} = Data[i];
+
+            if(values.username === username){
+                setField({usernameField: "success" , passwordField: "error"});
+                
+
+                if(values.password === password) {
+                    setField({usernameField: "success" , passwordField: "success"});
+                    console.log(allFieldsRight);
+                }
+                break;
+            } else {
+                setField({usernameField: "error" , passwordField: "error"});
+                console.log(allFieldsRight);
+            }
+        }
     }
-    
+
+    useEffect(() => {
+        
+            if(allFieldsRight.usernameField === "success" && allFieldsRight.passwordField === "success") {
+                        
+                login();
+                history.push('/landingpage');
+            }
+        }
+    )
+
+    const controlField = (e) => {
+        const value = e.target.value;
+        const id = e.target.getAttribute('id');
+        setValues((prevState) => {
+            return({
+                ...prevState,
+                [id] : value
+            })
+        });
+        console.log(values)
+        setField({usernameField: null , passwordField: null});
+    }
+
     return (
         <section className="login-page">
             <h2>click here to see the landing page</h2>
-            <button onClick={userLogin}>click me</button>
+            <form>
+                <div className={allFieldsRight.usernameField === "error" ? ("form-group error") : ("form-group")}>
+                    <input type="text" id="username" placeholder="ENTER THE USERNAME" onChange={controlField} value={values.username} />
+                    <span>please enter the valid user name</span>
+                </div>
+                <div className={allFieldsRight.passwordField === "error" ? ("form-group error") : ("form-group")}>
+                    <input type="password" id="password" placeholder="ENTER THE PASSWORD" onChange={controlField} value={values.password}/>
+                    <span>please enter the correct password</span>
+                </div>
+                <div className="form-control">
+                    <button onClick={userLogin} type="submit">click me</button>
+                </div>
+            </form>
         </section>
     )
 }
